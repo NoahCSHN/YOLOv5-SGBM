beb8b7c93cce6aa3ec75b2a4b1e55aacd2192ab3
NoahCSHN <yichongyangbit@163.com>
2021-06-11 16:37:57 +0800

    启动脚本修改

--- 2_start_up.sh
+++ 2_start_up.sh
@@ -3,5 +3,5 @@
 
 cd ~/ros_ws/src/avoid_model_port/scripts
 
-source ~/venv/torch/bin/activate
-python detect.py --visual --webcam --source 4 --cam_type 3 --sm_numdi 43 --cam_type 3 --sm_UniRa 5 --sm_mindi -5 --sm_block 9 --sm_tt 10 --sm_pfc 63 --sm_sws 100 --sm_sr 2 --score 0.6 --sm_d12md 1
+source ~/venv/cp37/bin/activate
+python detect.py --tcp_ip 192.168.2.75 --fps 4 --visual --webcam --source 4 --cam_type 3 --sm_numdi 43 --sm_UniRa 5 --sm_mindi -5 --sm_block 9 --sm_tt 10 --sm_pfc 63 --sm_sws 100 --sm_sr 2 --score 0 --sm_d12md 1
--- 3_data_record.sh
+++ 3_data_record.sh
@@ -2,5 +2,5 @@
 # start avoid model
 cd ~/ros_ws/src/avoid_model_port/scripts
 
-source ~/venv/torch/bin/activate
-python raw_record.py --source 4 --fps 30 --cam_type 3
+source ~/venv/cp37/bin/activate
+python raw_record.py --source 4 --fps 4 --cam_type 3
