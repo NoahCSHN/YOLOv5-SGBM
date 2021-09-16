1b0e2daab2a0a7a3c5499badc07ded49710bb0f7
NoahCSHN <yichongyangbit@163.com>
2021-09-16 08:52:10 +0800

    重新校正摄像头，添加摄像头校正参数验证脚本cam_calib.py

---
+++ cam_calib.py
@@ -0,0 +1,12 @@
+'''
+Author: Noah
+Date: 2021-09-16 08:41:49
+LastEditTime: 2021-09-16 08:51:08
+LastEditors: Please set LastEditors
+Description: Get Q matrix
+FilePath: /AI_SGBM/utils/cam_calib.py
+'''
+from utils.stereoconfig import stereoCamera
+
+if __name__ == "__main__":
+    cam=stereoCamera(3,416,416)
